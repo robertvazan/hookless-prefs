55196ac27d7c261d73d3c79ab2d8400cde21edbe
Robert Važan <robert.vazan@tutanota.com>
2023-04-11 07:58:03 +0000

    Scaffolding for the adapter

--- scripts/configure.py
+++ scripts/configure.py
@@ -5,14 +5,15 @@ import rvscaffold as scaffold
 
 class Project(scaffold.Java):
     def script_path_text(self): return __file__
-    def repository_name(self): return 'hookless'
+    def repository_name(self): return 'hookless-prefs'
     def is_member_project(self): return True
-    def pretty_name(self): return 'Hookless'
-    def pom_description(self): return 'Reactive programming library.'
+    def pretty_name(self): return 'Reactive preferences for Hookless'
+    def pom_description(self): return 'Hookless reactive adapter for Java preferences from java.util.prefs package.'
     def inception_year(self): return 2015
     def jdk_version(self): return 17
     def stagean_annotations(self): return True
     def complete_javadoc(self): return False
+    def has_website(self): return False
     
     def dependencies(self):
         yield from super().dependencies()
@@ -27,15 +28,9 @@ class Project(scaffold.Java):
         yield self.use('org.awaitility:awaitility:4.0.3', 'test')
         yield self.use('org.junit-pioneer:junit-pioneer:0.9.0')
         yield self.use_slf4j_test()
-    
+
     def javadoc_links(self):
         yield from super().javadoc_links()
-        yield 'https://noexception.machinezoo.com/javadocs/core/'
-        # No link to OpenTracing, because automatic modules cannot be linked.
-    
-    def documentation_links(self):
-        yield from super().documentation_links()
-        yield 'Concepts', 'https://hookless.machinezoo.com/concepts'
-        yield 'Adapters', 'https://hookless.machinezoo.com/adapters'
+        yield 'https://hookless.machinezoo.com/javadocs/core/'
 
 Project().generate()
